41e66bcfca72476f3a328c412f7f84e803eca9ff
shuto niimi <71388489+ooitanojohn@users.noreply.github.com>
2021-12-23 23:13:25 +0900

    useEffect消したfetchのみ

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 
 // import * as React from 'react'
 import * as React from 'react'
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import './App.css'
 
 /**
@@ -18,61 +18,56 @@ export const App = () => {
     'https://images.dog.ceo/breeds/dhole/n02115913_4117.jpg',
   )
   // ajax
-  const randImg = () => {
-    useEffect(() => {
-      fetch('https://dog.ceo/api/breeds/image/random')
-        .then(res => res.json())
-        .then(
-          result => {
-            setIsLoaded(true)
-            setDogUrl(result)
-          },
-          error => {
-            setIsLoaded(true)
-            setError(error)
-          },
-        )
-    }, [])
-  }
-  randImg()
-  if (error) {
-    return <div>Error: {error.message}</div>
-  } else if (!isLoaded) {
-    return <div>Loading...</div>
-  } else {
-    return (
-      <div>
-        <header>
-          <p>らっこのめいちゃん</p>
-        </header>
-        <main>
-          <p>らっこの可愛さをわからせる</p>
-          <img
-            src={dogUrl.message}
-            alt="らっこの画像"
-            width="300"
-            height="300"
-          />
-          <button
-            onClick={() => {
-              fetch('https://dog.ceo/api/breeds/image/random')
-                .then(res => res.json())
-                .then(
-                  result => {
-                    setIsLoaded(true)
-                    setDogUrl(result)
-                  },
-                  error => {
-                    setIsLoaded(true)
-                    setError(error)
-                  },
-                )
-            }}
-          >
-            更新
-          </button>
-        </main>
-      </div>
-    )
-  }
+  // const randImg = () => {
+  //   useEffect(() => {
+  //     fetch('https://dog.ceo/api/breeds/image/random')
+  //       .then(res => res.json())
+  //       .then(
+  //         result => {
+  //           setIsLoaded(true)
+  //           setDogUrl(result)
+  //         },
+  //         error => {
+  //           setIsLoaded(true)
+  //           setError(error)
+  //         },
+  //       )
+  //   }, [])
+  // }
+  // randImg()
+  // if (error) {
+  //   return <div>Error: {error.message}</div>
+  // } else if (!isLoaded) {
+  //   return <div>Loading...</div>
+  // } else {
+  return (
+    <div>
+      <header>
+        <p>らっこのめいちゃん</p>
+      </header>
+      <main>
+        <p>らっこの可愛さをわからせる</p>
+        <img src={dogUrl} alt="らっこの画像" width="300" height="300" />
+        <button
+          onClick={() => {
+            fetch('https://dog.ceo/api/breeds/image/random')
+              .then(res => res.json())
+              .then(
+                result => {
+                  setIsLoaded(true)
+                  setDogUrl(result.message)
+                },
+                error => {
+                  setIsLoaded(true)
+                  setError(error)
+                },
+              )
+          }}
+        >
+          更新
+        </button>
+      </main>
+    </div>
+  )
+  // }
 }
