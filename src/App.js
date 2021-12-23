30c3a0a3e945660c94819dde549ac95f2d304edd
shuto niimi <71388489+ooitanojohn@users.noreply.github.com>
2021-12-23 21:22:45 +0900

    state完全に理解した()

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // DO NOT DELETE
 
-import * as React from 'react'
+// import * as React from 'react'
+import React, { useState } from 'react'
 import './App.css'
 
 /**
@@ -8,6 +9,9 @@ import './App.css'
  * @type {React.FC}
  */
 export const App = () => {
+  const [dogUrl, setDogUrl] = useState(
+    'https://images.dog.ceo/breeds/dhole/n02115913_4117.jpg',
+  )
   return (
     <body>
       <header>
@@ -15,12 +19,7 @@ export const App = () => {
       </header>
       <main>
         <p>らっこの可愛さをわからせる</p>
-        <img
-          src="https://images.dog.ceo/breeds/dhole/n02115913_4117.jpg"
-          alt="らっこの画像"
-          width="300"
-          height="300"
-        />
+        <img src={dogUrl} alt="らっこの画像" width="300" height="300" />
       </main>
     </body>
   )
