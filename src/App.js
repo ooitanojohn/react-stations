6e87133c1994e315cc92ea296a273a76d626c080
shuto niimi <71388489+ooitanojohn@users.noreply.github.com>
2021-12-23 23:10:09 +0900

    useEffect周り難しい

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 // DO NOT DELETE
 
 // import * as React from 'react'
-import React, { useState } from 'react'
+import * as React from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 /**
@@ -9,27 +10,69 @@ import './App.css'
  * @type {React.FC}
  */
 export const App = () => {
+  // fetch state
+  const [error, setError] = useState(null)
+  const [isLoaded, setIsLoaded] = useState(false)
+  // state
   const [dogUrl, setDogUrl] = useState(
     'https://images.dog.ceo/breeds/dhole/n02115913_4117.jpg',
   )
-  return (
-    <body>
-      <header>
-        <p>らっこのめいちゃん</p>
-      </header>
-      <main>
-        <p>らっこの可愛さをわからせる</p>
-        <img src={dogUrl} alt="らっこの画像" width="300" height="300" />
-        <button
-          onClick={() =>
-            setDogUrl(
-              'https://images.dog.ceo/breeds/hound-english/n02089973_1132.jpg',
-            )
-          }
-        >
-          更新
-        </button>
-      </main>
-    </body>
-  )
+  // ajax
+  const randImg = () => {
+    useEffect(() => {
+      fetch('https://dog.ceo/api/breeds/image/random')
+        .then(res => res.json())
+        .then(
+          result => {
+            setIsLoaded(true)
+            setDogUrl(result)
+          },
+          error => {
+            setIsLoaded(true)
+            setError(error)
+          },
+        )
+    }, [])
+  }
+  randImg()
+  if (error) {
+    return <div>Error: {error.message}</div>
+  } else if (!isLoaded) {
+    return <div>Loading...</div>
+  } else {
+    return (
+      <div>
+        <header>
+          <p>らっこのめいちゃん</p>
+        </header>
+        <main>
+          <p>らっこの可愛さをわからせる</p>
+          <img
+            src={dogUrl.message}
+            alt="らっこの画像"
+            width="300"
+            height="300"
+          />
+          <button
+            onClick={() => {
+              fetch('https://dog.ceo/api/breeds/image/random')
+                .then(res => res.json())
+                .then(
+                  result => {
+                    setIsLoaded(true)
+                    setDogUrl(result)
+                  },
+                  error => {
+                    setIsLoaded(true)
+                    setError(error)
+                  },
+                )
+            }}
+          >
+            更新
+          </button>
+        </main>
+      </div>
+    )
+  }
 }
